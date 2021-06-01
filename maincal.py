5184e75322906483c51d265a4ed91cc07e5110d9
Shahil Dhakal <dshahil548@gmail.com>
2021-06-01 08:54:20 +0545

    Git work ignore files managed

--- maincal.py
+++ maincal.py
@@ -6,9 +6,9 @@ import sub_work
 a=100
 b=50
 
-total_sum=add_work.add(a,b)
-total_sub=sub_work.sub(a,b)
-total_mul=mul_work.mul(a,b)
-total_div=div_work.div(a,b)
+total_sum= add_work.add(a,b)
+total_sub= sub_work.sub(a,b)
+total_mul= mul_work.mul(a,b)
+total_div= div_work.div(a,b)
 
 print(f"Total Sum is {total_sum}. Total Sub is {total_sub}. Total Mul is {total_mul}. Total div is {total_div}.")
